8d54d4b635c8d2c6e0de24511a09ce73a1ca762e
Luke <lkpttn@gmail.com>
2015-10-09 21:46:30 -0400

    winCircle color theme support

--- Conjure/winCircle.swift
+++ Conjure/winCircle.swift
@@ -10,9 +10,8 @@ import UIKit
 
 class winCircle: UIView {
     
-    // MARK: Properties
-    // let rect: CGRect
-    
+    var defaults = NSUserDefaults.standardUserDefaults()
+    var fillColor = UIColor()
     
     // MARK: Initialization
     override init(frame: CGRect) {
@@ -27,9 +26,35 @@ class winCircle: UIView {
     // Override the drawing code to make the view a yellow rectangle.
     override func drawRect(rect: CGRect) {
         // Create a bezier path that is limited to an oval.
+        let selectedTheme = defaults.stringForKey("selectedTheme")
+        changeTheme(selectedTheme!)
+        
         let path = UIBezierPath(ovalInRect: rect)
-        UIColor.yellowColor().setFill()
+        fillColor.setFill()
         path.fill()
+    }
+    
+    func changeTheme(selectedTheme: String) {
+        switch selectedTheme {
+        case "Beleren":
+            fillColor = UIColor.yellowColor()
+        case "Aggro":
+            fillColor = UIColor.whiteColor()
+        case "Nissa":
+            fillColor = UIColor.yellowColor()
+        case "Aeons Torn":
+            fillColor = UIColor.purpleColor()
+        case "Scalding":
+            fillColor = UIColor.whiteColor()
+        case "Scapeshift":
+            fillColor = UIColor.whiteColor()
+        case "Living End":
+            fillColor = UIColor.whiteColor()
+        case "Golden":
+            fillColor = UIColor.whiteColor()
+        default:
+            fillColor = UIColor.yellowColor()
+        }
     }
 
 }
