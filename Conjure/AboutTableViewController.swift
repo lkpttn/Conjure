ccd4db954077f551ec0473d25408f4bcba84ee4d
Luke <lkpttn@gmail.com>
2015-10-20 15:12:55 -0400

    Fixed a lot of stuff on the intro view and about page
    Both are now entirely setup with an image and reflect the current
    theme. Links on the about page now successfully link out.

---
+++ Conjure/AboutTableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  AboutTableViewController.swift
+//  Conjure
+//
+//  Created by Luke Patton on 10/20/15.
+//  Copyright © 2015 Luke Patton. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class AboutTableViewController: UITableViewController, MFMailComposeViewControllerDelegate {
+    
+    let linkArray = ["http://friendofpixels.com","https://twitter.com/friendofpixels", "email"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    // MARK: - Table view data source
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView,
+        forSection section: Int) {
+            let header = view as! UITableViewHeaderFooterView
+            header.textLabel!.font = UIFont(name: "SourceSansPro-Regular", size: 13)
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+        let url = linkArray[indexPath.row]
+        if url != "email" {
+            UIApplication.sharedApplication().openURL(NSURL(string: url)!)
+        } else {
+            let mailComposeViewController = configuredMailComposeViewController()
+            if MFMailComposeViewController.canSendMail() {
+                self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+            } else {
+                self.showSendMailErrorAlert()
+            }
+        }
+    }
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["lkpttn@gmail.com"])
+        mailComposerVC.setSubject("Conjure")
+        mailComposerVC.setMessageBody("Conjure is my favorite app but....", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
